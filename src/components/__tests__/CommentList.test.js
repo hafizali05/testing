f94e4000a5f658889a96c722d4bf61656f34e7e3
hafizali05 <hafizdidarali@hotmail.com>
2019-05-28 08:29:23 +0100

    testing all the list of comment list component

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -21,3 +21,7 @@ it('creates one LI per comment', () => {
   expect(wrapped.find('li').length).toEqual(2);
 });
 
+it('shows the text for each comment', () => {
+  expect(wrapped.render().text()).toContain('Comment 1');
+  expect(wrapped.render().text()).toContain('Comment 2');
+});
