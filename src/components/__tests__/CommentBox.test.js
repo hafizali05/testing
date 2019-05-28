9b5ba6e9c0a321522f95c682a6fd7444f397e64a
hafizali05 <hafizdidarali@hotmail.com>
2019-05-28 08:57:47 +0100

    updated comment box test for the change done

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -13,10 +13,10 @@ beforeEach(() => {
     </Root>
   );
 });
-it("has a text area and a button", () => {
+it("has a text area and 2 buttons", () => {
   // const wrapper = mount(<CommentBox />);
   expect(wrapper.find("textarea").length).toEqual(1);
-  expect(wrapper.find("button").length).toEqual(1);
+  expect(wrapper.find("button").length).toEqual(2);
 });
 
 describe("the text area", () => {
